767f3cf6cfb9d17e1322240cf1f6409b84ab720a
z4kkyy <mathtaka1528@gmail.com>
2025-05-28 07:52:57 +0900

    Add NotificationHandler

---
+++ NotificationHandler/NotificationExample.cs
@@ -0,0 +1,30 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class NotificationExample : UdonSharpBehaviour
+{
+    [SerializeField] private NotificationHandler notificationHandler;
+    private int notificationCount = 0;
+
+    public override void Interact()
+    {
+        base.Interact();
+        notificationHandler.HideNotification();
+        
+        int val = notificationCount % 3;
+        switch (val)
+        {
+            case 0:
+                notificationHandler.HandleNotification(NotificationType.Type1, "This is a Type 1 notification.");
+                break;
+            case 1:
+                notificationHandler.HandleNotification(NotificationType.Type2, "This is a Type 2 notification.");
+                break;
+            default:
+                break;
+        }
+        notificationCount++;
+    }
+}
