9c71154f6ccf99bfc1d1726cd4abf6e7431c3cbf
z4kkyy <mathtaka1528@gmail.com>
2025-07-05 05:13:51 +0900

    refactor: remove namespace and add SerializeField attributes

--- SupportersList/AutoScrollRect.cs
+++ SupportersList/AutoScrollRect.cs
@@ -4,95 +4,85 @@ using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
-namespace mahu.games
+public class AutoScrollRect : UdonSharpBehaviour
 {
+    [SerializeField] public float timeAtTop = 5f;
+    [SerializeField] public float unitsPerSec = 20f;
+    [SerializeField] public float timeAtBottom = 3f;
 
-    public class AutoScrollRect : UdonSharpBehaviour
+    private float t;
+    private ScrollState state;
+    private ScrollRect scrollRect;
+    private float unitsToScroll;
+    private float progressPerSec;
+
+    private void Start()
     {
-        public float timeAtTop = 5f;
+        scrollRect = GetComponent<ScrollRect>();
+    }
 
-        public float unitsPerSec = 20f;
-
-        public float timeAtBottom = 3f;
-
-        private float t;
-
-        private ScrollState state;
-
-        private ScrollRect scrollRect;
-
-        private float unitsToScroll;
-
-        private float progressPerSec;
-
-        private void Start()
+    private void Update()
+    {
+        if (Time.frameCount % 100 == 0)
         {
-            scrollRect = GetComponent<ScrollRect>();
+            unitsToScroll = scrollRect.content.rect.height - scrollRect.viewport.rect.height;
+            progressPerSec = unitsPerSec / Mathf.Max(unitsToScroll, 0.001f);
         }
 
-        private void Update()
+        if (unitsToScroll <= 0)
         {
-            if (Time.frameCount % 100 == 0)
-            {
-                unitsToScroll = scrollRect.content.rect.height - scrollRect.viewport.rect.height;
-                progressPerSec = unitsPerSec / Mathf.Max(unitsToScroll, 0.001f);
-            }
+            return;
+        }
 
-            if (unitsToScroll <= 0)
-            {
-                return;
-            }
+        t += Time.deltaTime;
 
-            t += Time.deltaTime;
-
-            if (state == ScrollState.StoppedTop)
+        if (state == ScrollState.StoppedTop)
+        {
+            if (t > timeAtTop)
             {
-                if (t > timeAtTop)
-                {
-                    t = 0;
-                    state = ScrollState.ScrollDown;
-                }
+                t = 0;
+                state = ScrollState.ScrollDown;
             }
-            else if (state == ScrollState.StoppedBottom)
+        }
+        else if (state == ScrollState.StoppedBottom)
+        {
+            if (t > timeAtBottom)
             {
-                if (t > timeAtBottom)
-                {
-                    t = 0;
-                    state = ScrollState.ScrollUp;
-                }
+                t = 0;
+                state = ScrollState.ScrollUp;
             }
-            else if (state == ScrollState.ScrollDown)
+        }
+        else if (state == ScrollState.ScrollDown)
+        {
+            if (scrollRect.verticalNormalizedPosition >= 1)
             {
-                if (scrollRect.verticalNormalizedPosition >= 1)
-                {
-                    t = 0;
-                    state = ScrollState.StoppedBottom;
-                }
-                else
-                {
-                    scrollRect.verticalNormalizedPosition += progressPerSec * Time.deltaTime;
-                }
+                t = 0;
+                state = ScrollState.StoppedBottom;
             }
             else
             {
-                if (scrollRect.verticalNormalizedPosition <= 0)
-                {
-                    t = 0;
-                    state = ScrollState.StoppedTop;
-                }
-                else
-                {
-                    scrollRect.verticalNormalizedPosition -= progressPerSec * Time.deltaTime;
-                }
+                scrollRect.verticalNormalizedPosition += progressPerSec * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (scrollRect.verticalNormalizedPosition <= 0)
+            {
+                t = 0;
+                state = ScrollState.StoppedTop;
+            }
+            else
+            {
+                scrollRect.verticalNormalizedPosition -= progressPerSec * Time.deltaTime;
             }
         }
     }
+}
 
-    internal enum ScrollState
-    {
-        StoppedTop,
-        ScrollDown,
-        StoppedBottom,
-        ScrollUp
-    }
+internal enum ScrollState
+{
+    StoppedTop,
+    ScrollDown,
+    StoppedBottom,
+    ScrollUp
 }
